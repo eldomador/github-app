6d4fe7710fc0e8644c9c4a3bcbbd6cf52ab3edc5
eldomador <johncenajakub@gmail.com>
2021-11-09 16:16:45 +0100

    fix: getUser add: form EventListener

--- script.js
+++ script.js
@@ -6,32 +6,42 @@ const search = document.getElementById("search");
 
 async function getUser(user) {
   const resp = await fetch(APIURL + user);
-  const respData = await resp.jeson();
+  const respData = await resp.json();
 
   createUserCard(respData);
 }
 
 function createUserCard(user) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-
   const cardHTML = `
-<div class="card">
-  <div>
-    <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
-  </div>
-  <div class="user-info">
-    <h2>${user.name}</h2>
-    <p>${user.bio}</p>
-    <ul class="info">
-      <li>${user.followers}<strong>Followers</strong></li>
-      <li>${user.following}<strong>Following</strong></li>
-      <li>${user.public_repos}<strong>Repos</strong></li>
-    </ul>
-    <div id="repos"></div>
-  </div>
-</div>
-`;
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul class="info">
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                </ul>
+                <div id="repos"></div>
+            </div>
+        </div>
+    `;
 
-main.innerHTML = cardHTML;
+  main.innerHTML = cardHTML;
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = "";
+  }
+  console.log(user);
+});
